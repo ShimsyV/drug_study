26e5ab08f528d360f46211bde1145fa0adb6f833
Sam Jebreen <jebreensa@gmail.com>
2020-12-10 23:43:42 -0600

    update graph 2

--- static/js/graph2.js
+++ static/js/graph2.js
@@ -39,13 +39,13 @@ function getState2()
         //   operation: '=',
         //   value: 'July'
         },
-        {
-            type: 'aggregate',
-            groups: unpack(rows2, 'month'),
-            aggregations: [
-            {target: 'y', func: 'avg'}
-            ]
-        }
+        // {
+        //     type: 'aggregate',
+        //     groups: unpack(rows2, 'month'),
+        //     aggregations: [
+        //     {target: 'y', func: 'avg'}
+        //     ]
+        // }
         ]
         }]
       
@@ -99,13 +99,13 @@ function getYear2()
         //   operation: '=',
         //   value: 'July'
         },
-        {
-            type: 'aggregate',
-            groups: unpack(rows, 'month'),
-            aggregations: [
-            {target: 'y', func: 'avg'},
-            ]
-        }
+        // {
+        //     type: 'aggregate',
+        //     groups: unpack(rows, 'month'),
+        //     aggregations: [
+        //     {target: 'y', func: 'avg'},
+        //     ]
+        // }
         ]
         }]
       
